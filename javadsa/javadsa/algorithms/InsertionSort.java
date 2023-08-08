ea8c6dad7f453c8b9abb83178c9a073ddc560f23
Felipe <felipeuberaba10@gmail.com>
2023-08-07 23:10:43 -0300

    File organization (packages dont make sense)

---
+++ javadsa/javadsa/algorithms/InsertionSort.java
@@ -0,0 +1,23 @@
+package javadsa.algorithms;
+
+public class InsertionSort {
+    public static int[] insertionSort(int[] array) {
+        int key;
+        
+        for (int i = 1; i < array.length; i++) {
+            key = array[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j++;
+            }
+
+            array[j + 1] = key;
+        }
+
+        return array;
+    }
+
+}
